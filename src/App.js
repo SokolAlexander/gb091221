0c1c157d1ad3af5a04bfc417d58d6cbf4ba0ecf9
SokolAlexander <asokol1994@gmail.com>
2021-12-09 21:51:13 +0300

    lesson1 example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, ButtonClass } from "./components/Button/Button";
 
+const name = "Alex";
+const className = "App-header";
 function App() {
+  const buttonLabel = "BUTTON";
+
+  const handleClick = (text) => {
+    alert(`Hello from btn: ${text}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={className}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>HELLO {name}</h3>
+        <Button onButtonClick={handleClick} title={buttonLabel} anyProp={123} />
       </header>
     </div>
   );
 }
 
+const element = <h1>REACT</h1>;
+
 export default App;
+
+const foo = {
+  x: 1,
+  y: 2,
+};
+
+console.log({ ...foo, y: 4 });
